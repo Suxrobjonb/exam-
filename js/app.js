3017d1b9a28ce9a7e0fe2a02c3fa41970d49459c
Suxrobjonb <suxrobjonb6@gmail.com>
2022-10-29 02:55:26 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// ulanishi
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".input__dictionary");
+let elSearchBtn = document.querySelector(".site__search");
+let elResaultBox = document.querySelector(".box__resault") 
+let commitRes = document.querySelector(".about__the-result");
+let ReadingEl = document.querySelector(".site__resault-title");
+let elExample = document.querySelector(".exaple");
+let elAudio = document.querySelector(".audio__sound");
+
+elForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    // backend
+     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${elInput.value}`)
+     .then(response => response.json())
+     .then(data => {
+        data.forEach(resaultEl => {
+            // lug'atni aytilishi
+            commitRes.innerHTML = resaultEl.meanings[0].definitions[0].definition;
+            // ta'rifi
+            ReadingEl.innerHTML =  resaultEl.word + " - " + resaultEl.phonetics[1].text.split('').slice(1, -1).join('');
+            // audiosi
+            elAudio.setAttribute("controls", "")
+            elAudio.src = element.phonetics[0].audio;
+            console.log(resaultEl);
+            
+          });       
+     })                         
+    })
+
+    
+
